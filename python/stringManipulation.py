95746e2ad54ef761b4a0feff7f698814eeba47c1
Johnny Packard <packard81@me.com>
2018-09-06 10:17:06 -0500

    string manipulation trimming

---
+++ python/stringManipulation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+strUsers = 'jpackard  .   rrojas, dsiemers,  wpackard   , lmorningstar   '
+arrUsers = strUsers.split(',')
+
+for user in arrUsers:
+    trimUser = user.strip()
+    trimUserR = user.rstrip()
+    trimUserL = user.lstrip()
+
+    firstInitial = trimUser[:1]
+    lastInitial = trimUser[1:2]
+    lastName = trimUser[1:]
+
+    print 'User : \'' + user + '\''
+    print 'LTrim: \'' + trimUserL + '\''
+    print 'RTrim: \'' + trimUserR + '\''
+    print ' Trim: \'' + trimUser + '\''
+
+    print 'First Initial:', firstInitial.upper()
+    print 'Last Initial: ', lastInitial.upper()
+    print 'Last Name:', lastName
+
+    print ''
